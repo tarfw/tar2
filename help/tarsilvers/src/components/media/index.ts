284daf16fe3ec264c9a0128e6831b57dd7ae8473
tarfw <tarframework@outlook.com>
2025-09-23 21:51:20 +0530

    Remove help/tarsilvers from git tracking

--- help/tarsilvers/src/components/media/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MediaPicker } from './picker';
-export { default as MediaGallery } from './gallery';
-export { default as MediaManager } from './manager';
-export type { MediaItem } from './gallery';
